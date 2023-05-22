d07aa177e73a0a99dafe4647855f2ee970f2850d
shivansh sharma <79299523+shivansh-sharma1309@users.noreply.github.com>
2023-05-22 11:12:43 +0530

    Update matplotlib.py

--- matplotlib.py
+++ matplotlib.py
@@ -1,7 +1,2 @@
-ques = 'do you like pizza or burger?'
-ans = input(ques)
-if ans == 'burger':
-    print('order burger for you ')
-else : 
-    print("order burger for you ")
+print("hello from browser")
 
